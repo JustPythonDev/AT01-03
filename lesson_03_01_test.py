a5f71e525bfb2a19a971237349dbd21fe8229d42
Julia <JustPythonDev@ya.ru>
2024-10-07 13:38:53 +0300

    lesson_03

---
+++ lesson_03_01_test.py
@@ -0,0 +1,65 @@
+# PIP install pytest-mock
+from lesson_03_01 import get_weather, get_github_user
+
+
+def test_get_weather_success(mocker):
+   mock_get = mocker.patch('lesson_03_01.requests.get')
+
+   # Создаем мок-ответ для успешного запроса
+   mock_get.return_value.status_code = 200
+   mock_get.return_value.json.return_value = {
+       'weather': [{'description': 'clear sky'}],
+       'main': {'temp': 282.55}
+   }
+
+   api_key = 'test_api_key'
+   city = 'London'
+   weather_data = get_weather(api_key, city)
+
+   assert weather_data == {
+       'weather': [{'description': 'clear sky'}],
+       'main': {'temp': 282.55}
+   }
+
+
+def test_get_weather_failure(mocker):
+   mock_get = mocker.patch('lesson_03_01.requests.get')
+
+   # Создаем мок-ответ для неуспешного запроса
+   mock_get.return_value.status_code = 404
+
+   api_key = 'test_api_key'
+   city = 'UnknownCity'
+   weather_data = get_weather(api_key, city)
+
+   assert weather_data is None
+
+
+def test_get_github_user(mocker):
+   mock_get = mocker.patch('lesson_03_01.requests.get')
+   mock_get.return_value.status_code = 200
+   mock_get.return_value.json.return_value = {
+       'login' : 'nizavr',
+       'id' : 345178,
+       'name' : 'Nina'
+   }
+
+   user_data = get_github_user('cat')
+   assert user_data == {
+       'login' : 'nizavr',
+       'id' : 345178,
+       'name' : 'Nina'
+   }
+
+
+def test_get_github_user_with_error(mocker):
+   mock_get = mocker.patch('lesson_03_01.requests.get')
+   mock_get.return_value.status_code = 500
+
+   user_data = get_github_user('cat')
+   assert user_data == None
+
+
+
+
+
